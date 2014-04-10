ccf1ebc0d88f130dac716e06b38f1c06a70f2bcf
staticgit <ryan@digitalerosion.com>
2014-04-09 23:36:05 -0600

    heroku database ugliness

--- helloghz/helloghz/settings.py
+++ helloghz/helloghz/settings.py
@@ -155,7 +155,7 @@ LOGGING = {
     }
 }
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
