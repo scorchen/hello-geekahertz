bb70816694eb0248457d569b092675acb25f689c
staticgit <ryan@digitalerosion.com>
2014-04-09 23:03:49 -0600

    heroku database ugliness

--- helloghz/helloghz/settings.py
+++ helloghz/helloghz/settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'helloDB',                      # Or path to database file if using sqlite3.
+        'NAME': 'dc4koq4endvj0d',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'hello',
-        'PASSWORD': 'ghz',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'wdvzydruuerhge',
+        'PASSWORD': '4J4q3Yfiy-GaHQF8awHkOWfMN2',
+        'HOST': 'ec2-107-20-214-225.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
