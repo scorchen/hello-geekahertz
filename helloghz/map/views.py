ed20d1ed9fef331e5cac1eef70e77149c01efdfc
scorchen <scorchen@gmail.com>
2014-04-10 03:27:19 -0600

    More Styling addtions
    also added a method for parsing youtube video URL

--- helloghz/map/views.py
+++ helloghz/map/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from map.models import Point
 from map.forms import helloForm
+from urlparse import urlparse
+
 
 
 #index just returns list of points for javascript to map
@@ -17,8 +19,11 @@ def sayhello(request):
 
     if request.method == 'POST': # If the form has been submitted...
         form = helloForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            newPoint = Point(vid=form.cleaned_data['vid'],lat=form.cleaned_data['lat'],long=form.cleaned_data['long'])
+        if canPostData(request.POST): # All validation rules pass
+
+            videoId = getVideoId(form.cleaned_data['full_url'])
+
+            newPoint = Point(vid=videoId, lat=form.cleaned_data['lat'], long=form.cleaned_data['long'])
 
             newPoint.save()
             # ...
@@ -30,3 +35,35 @@ def sayhello(request):
         'form': form,
     })
 
+def getVideoId(fullUrl):
+    """
+    returns video id when given a youtube url
+    Examples:
+    - http://youtu.be/SA2iWivDJiE
+    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    - http://www.youtube.com/embed/SA2iWivDJiE
+    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(fullUrl)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    # fail?
+    return None
+
+def canPostData(postedData):
+    form = helloForm(postedData)
+    if not form.is_valid():
+        return False
+    videoId = str(getVideoId(form.cleaned_data['full_url']))
+    if videoId is None:
+        return False
+
+    return True
