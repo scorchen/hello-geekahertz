e9337ac0218618ddda2cf31c6adefd63b0c8c0f2
scorchen <scorchen@gmail.com>
2014-04-25 02:31:02 -0600

    more popup

--- helloghz/map/static/map/scripts/page.js
+++ helloghz/map/static/map/scripts/page.js
@@ -10,16 +10,19 @@ function initializeJQueryCode(rootselector) {
     $('select[data-val-required]', rootselector).addClass('required-field').each(function () { $("label[for='" + $(this).attr('id') + "']").addClass('required-field'); });
     $('textarea[data-val-required]', rootselector).addClass('required-field').each(function () { $("label[for='" + $(this).attr('id') + "']").addClass('required-field'); });
     $('.image-link').magnificPopup({ type: 'image' });
-    $('.video-popup').magnificPopup({ type: 'iframe', closeBtnInside: false, closeOnContentClick: true, });
-
+  
+    $('.video-popup').magnificPopup({
+        type: 'iframe',
+        closeOnContentClick: true,
+        gallery: {
+            enabled: true
+        }
+    });
+   
     $('.video').magnificPopup({
-
+        type: 'iframe',
         iframe: {
-            markup: '<div class="mfp-iframe-scaler">' +
-                      '<div class="mfp-close"></div>' +
-                      '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-                    '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
+          
             patterns: {
                 youtube: {
                     index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
@@ -30,22 +33,13 @@ function initializeJQueryCode(rootselector) {
                     // id: function(url) { return 'parsed id'; } 
 
                     src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe. 
-                },
-                vimeo: {
-                    index: 'vimeo.com/',
-                    id: '/',
-                    src: '//player.vimeo.com/video/%id%?autoplay=1'
-                },
-                gmaps: {
-                    index: '//maps.google.',
-                    src: '%id%&output=embed'
                 }
 
                 // you may add here more sources
 
             },
 
-            //srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+            srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
         }
 
 
